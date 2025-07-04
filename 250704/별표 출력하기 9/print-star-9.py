cc9a95520e40d2a1ccd2243658fd682fcc8b4471
leeyeaeun <105280327+leeyeaeun@users.noreply.github.com>
2025-07-04 15:54:02 +0900

    Title: 별표 출력하기 9, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250704/별표 출력하기 9/print-star-9.py
+++ 250704/별표 출력하기 9/print-star-9.py
@@ -1,8 +1,8 @@
 N = int(input())
 
 for i in range(N):
-    for j in range(N-i+1):
-        print(" ", end="")
-    for j in range(i+1):
+    for j in range(2*(N-i)-2):
+        print("", end=" ")
+    for j in range(2*i+1):
         print("*", end = " ")
     print()
