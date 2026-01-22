64e5fe1902ae53482ebe5bb410c4bed56f4c9cc5
leeyeaeun <105280327+leeyeaeun@users.noreply.github.com>
2026-01-22 14:36:27 +0900

    Title: 중복되지 않는 정수 중 최대, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

--- 260122/중복되지 않는 정수 중 최대/max-of-unique-number.py
+++ 260122/중복되지 않는 정수 중 최대/max-of-unique-number.py
@@ -1,13 +1,14 @@
 n = int(input())
 nums = list(map(int, input().split()))
 
-# Please write your code here.
-max_val = -1
+count = [0] * (1001)
+
 for elem in nums:
-    if max_val == elem:
-        max_val = -1
-        continue
-    if max_val < elem :
-        max_val = elem
-    
+    count[elem] +=1
+
+max_val = -1
+for i, elem in enumerate(count):
+    if elem == 1:
+        if max_val < i :
+            max_val = i 
 print(max_val)
