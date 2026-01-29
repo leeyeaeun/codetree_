7e246cdfba7623ed1263b7dd7eed630cc211c167
leeyeaeun <105280327+leeyeaeun@users.noreply.github.com>
2026-01-29 17:18:24 +0900

    Title: 대각선으로 정수 채우기, Time: 50ms, Memory: 16MB, Status: Passed - Codetree

--- 260129/대각선으로 정수 채우기/diagonal-numbering.py
+++ 260129/대각선으로 정수 채우기/diagonal-numbering.py
@@ -4,12 +4,15 @@ n, m = map(int, input().split())
 arr = [[1]*m for _ in range(n)]
 num = 2
 
-for k in range(1,m):
+for k in range(1, m+n-1):
     for i in range(k+1):
-        if i >=n:
-           continue
-        arr[i][k-i] = num
-        num +=1
+        #print(num, i, k-i , k)
+        if i >= n or k-i >=m:
+           #print('no')
+           pass
+        else:
+            arr[i][k-i] = num
+            num +=1
     k +=1
 
 for row in arr:
