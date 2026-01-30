5e7bb674a95081cc29c455d326cd57318e06be0a
leeyeaeun <105280327+leeyeaeun@users.noreply.github.com>
2026-01-30 14:45:50 +0900

    Title: 공백을 기준으로 출력, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 260130/공백을 기준으로 출력/output-based-on-space.py
@@ -0,0 +1,13 @@
+a = input()
+b = input()
+
+
+for elem in a:
+    if elem == " ":
+        continue
+    print(elem, end="")
+
+for elem in b:
+    if elem == " ":
+        continue
+    print(elem, end="")
