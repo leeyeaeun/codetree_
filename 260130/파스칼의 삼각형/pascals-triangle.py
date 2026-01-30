70e89252c6252ae10f06d50fb326e98258d81310
leeyeaeun <105280327+leeyeaeun@users.noreply.github.com>
2026-01-30 13:42:53 +0900

    Title: 파스칼의 삼각형, Time: 41ms, Memory: 16MB, Status: Passed - Codetree

--- 260130/파스칼의 삼각형/pascals-triangle.py
+++ 260130/파스칼의 삼각형/pascals-triangle.py
@@ -1,13 +1,13 @@
 N = int(input())
 
-arr= [[1]*N for _ in range(N)]
+arr= [[1]*(N+1) for _ in range(N+1)]
 print('1')
-print('1 1')
-for i in range(1,N-1):
+#print('1 1')
+for i in range(N-1):
     print(1, end = " ")
     for j in range(i):
-        arr[i][j] = arr[i-1][j-1] + arr[i-1][j]
-        print(arr[i][j], end = " ")
+        arr[i+1][j+1] = arr[i][j] + arr[i][j+1]
+        print(arr[i+1][j+1], end = " ")
     print(1, end = " ")
     print()
 
