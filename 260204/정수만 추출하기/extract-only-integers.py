642abf7bf59b0f6db785cf3c53d8b45e7f50e1c9
leeyeaeun <105280327+leeyeaeun@users.noreply.github.com>
2026-02-04 17:56:35 +0900

    Title: 정수만 추출하기, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 260204/정수만 추출하기/extract-only-integers.py
+++ 260204/정수만 추출하기/extract-only-integers.py
@@ -1,7 +1,11 @@
 s= list(input().split())
 
 ans = 0
+
 for i in s:
+    if i.isdigit():
+        ans += int(i)
+        continue
     for j in i:
         if j.isdigit() == False:
             ind = i.index(j)
