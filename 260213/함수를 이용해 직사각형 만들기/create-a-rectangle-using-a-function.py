32dd18662ad385f850933519c11e31ad18b23090
leeyeaeun <105280327+leeyeaeun@users.noreply.github.com>
2026-02-13 16:40:08 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: 62ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 260213/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.py
@@ -0,0 +1,10 @@
+n, m = map(int, input().split())
+
+# Please write your code here.
+def print_rec(n, m):
+    for _ in range(n):
+        for _ in range(m):
+            print('1',end="")
+        print()
+
+print_rec(n,m)
