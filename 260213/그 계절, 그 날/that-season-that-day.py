122e0d8ea1a256fb312f9c778cd81e1d92f0e2c5
leeyeaeun <105280327+leeyeaeun@users.noreply.github.com>
2026-02-13 17:40:53 +0900

    Title: 그 계절, 그 날, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260213/그 계절, 그 날/that-season-that-day.py
@@ -0,0 +1,43 @@
+Y, M, D = map(int, input().split())
+
+# Please write your code here.
+
+def is_lunar_year(Y,M,D):
+    if Y % 4 == 0:
+        if Y %100 ==0:
+            if Y % 400:
+                return True
+            else : return False
+        else:
+            return True
+    else:
+        return False
+
+def is_real_day(Y,M,D):
+        if M in [1,3,5,7,8,10,12]:
+            if D>=1 and D <=31:
+                return True
+            else:
+                return False
+        else:
+            if D>=1 and D <=30 and M != 2:
+                return True
+            else:
+                if M == 2 and D == 29:
+                    if is_lunar_year(Y,M,D):
+                        return True
+                    else:
+                        return False
+                return False
+
+if is_real_day(Y,M,D):
+    if M>=3 and M<=5:
+        print('Spring')
+    elif M>=6 and M<=8:
+        print('Summer')
+    elif M>=9 and M<=11:
+        print('Fall')
+    else:
+        print('Winter')
+else:
+    print('-1')
