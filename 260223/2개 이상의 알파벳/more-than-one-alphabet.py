8377bdf43b8e917b89090a113c61cbcd343d0c09
leeyeaeun <105280327+leeyeaeun@users.noreply.github.com>
2026-02-23 17:59:31 +0900

    Title: 2개 이상의 알파벳, Time: 56ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 260223/2개 이상의 알파벳/more-than-one-alphabet.py
@@ -0,0 +1,14 @@
+A = input()
+
+# Please write your code here.
+def isDifferent(A):
+    arr = list(A)
+    s = set()
+    for elem in arr:
+        s.add(elem)
+    
+    if len(s) > 2:
+        return 'Yes'
+    return 'No'
+
+print(isDifferent(A))
